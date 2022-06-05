8f49aa793728211323f8feb5bcc17af9ef3e3d36
ibrahemaskar <74019633+ibrahemaskar11@users.noreply.github.com>
2022-06-05 17:53:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Layout/Home";
 import Search from "./components/Layout/Search";
-import {  useEffect, useContext } from "react";
+import { useEffect, useContext } from "react";
 import MyReadsContext from "./Store/MyReadsContext";
 import MyReadsProvider from "./Store/MyReadsProvider";
 
 const App = (props) => {
-  const {getMyBooks} = useContext(MyReadsContext)
+  const { getMyBooks } = useContext(MyReadsContext);
   //useEffect is used with on dependency to call getMyBooks as soon as the App is deployed
   useEffect(() => {
     getMyBooks();
@@ -16,13 +16,13 @@ const App = (props) => {
   return (
     //React Router is used to allow building a SPA application
     <Router>
-        {/*
+      {/*
           -there exists two routes in this application the home route which is denoted by '/' and the search route which is denoted by '/search/
         */}
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/search" element={<Search />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </Router>
   );
 };
