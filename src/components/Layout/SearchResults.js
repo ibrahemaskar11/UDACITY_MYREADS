753f1a5a3744a6e75ac5ccccdb38bcfcb5224d99
ibrahemaskar <74019633+ibrahemaskar11@users.noreply.github.com>
2022-06-05 17:40:00 +0200

    change in withinShelf name

--- src/components/Layout/SearchResults.js
+++ src/components/Layout/SearchResults.js
@@ -8,10 +8,10 @@ import Book from "../Books/Book";
 import classes from "./SearchResults.module.css";
 const SearchResults = (props) => {
   const { books } = useContext(MyReadsContext);
-  //if the book already exists in books in MyReadsContext then that book in the searchResults should be swapped with the object in the context to include the value of the book 
+  //if the book already exists in books in MyReadsContext then that book in the searchResults should be swapped with the object in the context to include the value of the book
   const modifiedSearchResults = props.searchResults.map((book) => {
-    const withinShelf = books.filter((myBook) => myBook.id === book.id);
-    return withinShelf ? withinShelf[0] : book;
+    const withinShelf = books.filter((el) => el.id === book.id);
+    return withinShelf.length ? withinShelf[0] : book;
   });
   //the searchResults passed by the component props is to be mapped into a list of books each with its own unique key
   const searchResultsContent = (
